e2e9f5f5981f39544771ceb0155c85412f0dc5e5
unknown <saad123mn123@gmail.com>
2023-11-27 22:13:20 +0200

    fix

--- app/[lng]/dashboard/[dashboard]/page.jsx
+++ app/[lng]/dashboard/[dashboard]/page.jsx
@@ -44,7 +44,20 @@ export async function generateStaticParams() {
 const RoutedPage = ({ currentPage }) => {
   if (!currentPage) {
     // Render a loading state or handle the absence of currentPage
-    return <div>Loading...</div>;
+    return 
+    <main className="dash">
+    <section className="sm:gap-4 sm:flex">
+      <NavBar lng={currentPage.lng} />
+      <section className="flex w-full sm:h-[98dvh]">
+        <section className="flex-col mt-16 sm:mt-4 bg-white rounded-xl w-full lg:w-[99%] pt-3 px-4 text-blue-950 text-3xl font-semibold relative">
+        <div className="flex justify-center items-center w-full h-full">
+          <div className="border-4 border-gray-200 rounded-full w-12 h-12 border-t-4 border-blue-500 animate-spin"></div>
+        </div>
+        </section>
+      </section>
+    </section>
+  </main>
+    ;
   }
 
   // Render the component based on the currentPage
