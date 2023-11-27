dcf27c8764f2e894cc3b9ea58bf733b23fe95589
unknown <saad123mn123@gmail.com>
2023-11-27 22:06:57 +0200

    fix

--- app/[lng]/dashboard/[dashboard]/page.jsx
+++ app/[lng]/dashboard/[dashboard]/page.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+// PageRenderer.js
+
 import Contracts from '@/components/pages/contracts.jsx';
 import Cars from '@/components/pages/cars';
 import Customers from '@/components/pages/customers.jsx';
@@ -8,7 +9,23 @@ import Maintenance from '@/components/pages/maintenance.jsx';
 import Treasury from '@/components/pages/treasury.jsx';
 import NavBar from '@/components/navBar';
 
+const PageRenderer = ({ currentPage }) => {
+  return (
+    <main className="dash">
+      <section className="sm:gap-4 sm:flex">
+        <NavBar lng={currentPage.lng} />
+        <section className="flex w-full sm:h-[98dvh]">
+          <section className="flex-col mt-16 sm:mt-4 bg-white rounded-xl w-full lg:w-[99%] pt-3 px-4 text-blue-950 text-3xl font-semibold relative">
+            {currentPage.component}
+          </section>
+        </section>
+      </section>
+    </main>
+  );
+};
+
 export async function generateStaticParams() {
+  // Define the possible dashboard pages
   const pages = [
     'cars',
     'contracts',
@@ -18,40 +35,20 @@ export async function generateStaticParams() {
     'treasury',
   ];
 
-  const staticPages = pages.map(page => ({ p: page }));
-  return staticPages;
+  // Generate params for each page
+  const params = pages.map((page) => ({ dashboard: page, lng: 'en' }));
+
+  return params;
 }
-const PageRenderer = ({params}) => {
-    const getPageComponent = () => {
-      switch (params.dashboard) {
-        case 'cars':
-          return <Cars lng={params.lng}/>;
-        case 'contracts':
-          return <Contracts lng={params.lng}/>;
-        case 'customers':
-          return <Customers lng={params.lng}/>;
-        case 'invoices':
-          return <Invoices lng={params.lng}/>;
-        case 'maintenance':
-          return <Maintenance lng={params.lng}/>;
-        case 'treasury':
-          return <Treasury lng={params.lng}/>;
-        default:
-          return null;
-      }
-    };
-    const currentPage = getPageComponent();
-    return (
-        <main className="dash">
-         <section className="sm:gap-4 sm:flex">
-            <NavBar lng={params.lng}/>
-            <section className="flex w-full sm:h-[98dvh]">
-              <section className="flex-col mt-16 sm:mt-4 bg-white rounded-xl w-full lg:w-[99%] pt-3 px-4 text-blue-950 text-3xl font-semibold relative">
-            {currentPage}
-            </section>
-          </section>
-        </section>
-      </main>
-    );
-  };
-export default PageRenderer;
+
+const RoutedPage = ({ currentPage }) => {
+  if (!currentPage) {
+    // Render a loading state or handle the absence of currentPage
+    return <div>Loading...</div>;
+  }
+
+  // Render the component based on the currentPage
+  return <PageRenderer currentPage={currentPage} />;
+};
+
+export default RoutedPage;
