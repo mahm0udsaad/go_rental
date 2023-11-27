f754d92812b00daf0a83993b3d488442b1413242
unknown <saad123mn123@gmail.com>
2023-11-27 22:29:47 +0200

    inhance

--- app/[lng]/dashboard/[dashboard]/page.jsx
+++ app/[lng]/dashboard/[dashboard]/page.jsx
@@ -1,5 +1,4 @@
 // PageRenderer.js
-
 import Contracts from '@/components/pages/contracts.jsx';
 import Cars from '@/components/pages/cars';
 import Customers from '@/components/pages/customers.jsx';
@@ -9,14 +8,33 @@ import Maintenance from '@/components/pages/maintenance.jsx';
 import Treasury from '@/components/pages/treasury.jsx';
 import NavBar from '@/components/navBar';
 
-const PageRenderer = ({ currentPage }) => {
+const PageRenderer = ({ params }) => {
+  const renderPageComponent = () => {
+    switch (params.dashboard) {
+      case 'cars':
+        return <Cars lng={params.lng} />;
+      case 'contracts':
+        return <Contracts lng={params.lng} />;
+      case 'customers':
+        return <Customers lng={params.lng} />;
+      case 'invoices':
+        return <Invoices lng={params.lng} />;
+      case 'maintenance':
+        return <Maintenance lng={params.lng} />;
+      case 'treasury':
+        return <Treasury lng={params.lng} />;
+      default:
+        return null;
+    }
+  };
+
   return (
     <main className="dash">
       <section className="sm:gap-4 sm:flex">
-        <NavBar lng={currentPage.lng} />
+        <NavBar lng={params.lng} />
         <section className="flex w-full sm:h-[98dvh]">
           <section className="flex-col mt-16 sm:mt-4 bg-white rounded-xl w-full lg:w-[99%] pt-3 px-4 text-blue-950 text-3xl font-semibold relative">
-            {currentPage.component}
+            {renderPageComponent()}
           </section>
         </section>
       </section>
@@ -41,27 +59,29 @@ export async function generateStaticParams() {
   return params;
 }
 
-const RoutedPage = ({ currentPage }) => {
-  if (!currentPage) {
-    // Render a loading state or handle the absence of currentPage
-    return 
-    <main className="dash">
-    <section className="sm:gap-4 sm:flex">
-      <NavBar lng={currentPage.lng} />
-      <section className="flex w-full sm:h-[98dvh]">
-        <section className="flex-col mt-16 sm:mt-4 bg-white rounded-xl w-full lg:w-[99%] pt-3 px-4 text-blue-950 text-3xl font-semibold relative">
-        <div className="flex justify-center items-center w-full h-full">
-          <div className="border-4 border-gray-200 rounded-full w-12 h-12 border-t-4 border-blue-500 animate-spin"></div>
-        </div>
+const RoutedPage = ({ params }) => {
+  if (!params) {
+    return (
+      <main className="dash">
+        <section className="sm:gap-4 sm:flex">
+          <NavBar lng={params?.lng} />
+          <section className="flex w-full sm:h-[98dvh]">
+            <section className="flex-col mt-16 sm:mt-4 bg-white rounded-xl w-full lg:w-[99%] pt-3 px-4 text-blue-950 text-3xl font-semibold relative">
+              <div className="flex justify-center items-center w-full h-full">
+              <div className="border-4 border-gray-200 rounded-full w-12 h-12 border-t-4 border-blue-500 animate-spin"></div>
+              </div>
+            </section>
+          </section>
         </section>
-      </section>
-    </section>
-  </main>
-    ;
+      </main>
+    );
   }
 
-  // Render the component based on the currentPage
-  return <PageRenderer currentPage={currentPage} />;
+  // Pass the params to the PageRenderer for rendering
+  return <PageRenderer params={params} />;
 };
 
 export default RoutedPage;
+
+
+
